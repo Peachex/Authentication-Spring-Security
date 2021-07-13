9f1a934879b5cbe5048e274531a3f72ff66c6995
Aleksey Klevitov <klevolex@gmail.com>
2021-07-13 23:25:16 +0300

    add constant

--- repository/src/main/java/com/epam/esm/constant/entity/UserFieldName.java
+++ repository/src/main/java/com/epam/esm/constant/entity/UserFieldName.java
@@ -3,6 +3,7 @@ package com.epam.esm.constant.entity;
 public class UserFieldName {
     public static final String ID = "id";
     public static final String EMAIL = "email";
+    public static final String ROLE = "role";
 
     private UserFieldName() {
     }
--- web/src/main/java/com/epam/esm/constant/HeaderName.java
+++ web/src/main/java/com/epam/esm/constant/HeaderName.java
@@ -2,6 +2,7 @@ package com.epam.esm.constant;
 
 public class HeaderName {
     public static final String LOCALE = "locale";
+    public static final String AUTHORIZATION = "auth";
 
     private HeaderName() {
     }
