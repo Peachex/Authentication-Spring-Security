b3924e74d71ae5d40c5f1c22f8ee98e48ac9c2a7
Aleksey Klevitov <klevolex@gmail.com>
2021-07-13 23:25:38 +0300

    add value from property file

--- web/src/main/java/com/epam/esm/security/JwtTokenProvider.java
+++ web/src/main/java/com/epam/esm/security/JwtTokenProvider.java
@@ -1,5 +1,7 @@
 package com.epam.esm.security;
 
+import com.epam.esm.constant.HeaderName;
+import com.epam.esm.constant.entity.UserFieldName;
 import com.epam.esm.constant.error.ErrorCode;
 import com.epam.esm.constant.error.ErrorName;
 import com.epam.esm.dto.User;
@@ -10,20 +12,27 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import javax.servlet.http.HttpServletRequest;
+import java.time.ZoneId;
 import java.util.Date;
 
 @Component
+@PropertySource("classpath:security/jwt.properties")
 public class JwtTokenProvider {
-    //private String secretKey;
-    //private int validityInMinutes; //todo read from property
+    private final SecretKey secretKey = MacProvider.generateKey();
     private final UserService<User> service;
+    @Value("${jwt.expirationInHours}")
+    private int expirationInHours;
 
     @Autowired
     public JwtTokenProvider(UserService<User> service) {
@@ -32,20 +41,21 @@ public class JwtTokenProvider {
 
     public String createToken(String email, String role) {
         Claims claims = Jwts.claims().setSubject(email);
-        claims.put("role", role);
+        claims.put(UserFieldName.ROLE, role);
         Date now = new Date();
-        Date validity = new Date(now.getTime() + 8 * 1000 * 60); //fixme add constant and change time
+        Date validity = Date.from(now.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+                .plusHours(expirationInHours).atZone(ZoneId.systemDefault()).toInstant());
         return Jwts.builder()
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, "proselyte")
+                .signWith(SignatureAlgorithm.HS256, secretKey)
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claimsJws = Jwts.parser().setSigningKey("proselyte").parseClaimsJws(token);
+            Jws<Claims> claimsJws = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
             return !claimsJws.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             throw new JwtAuthenticationException(ErrorCode.USER, ErrorName.INVALID_AUTH_TOKEN);
@@ -58,10 +68,10 @@ public class JwtTokenProvider {
     }
 
     public String getUserName(String token) {
-        return Jwts.parser().setSigningKey("proselyte").parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
     }
 
     public String resolveToken(HttpServletRequest request) {
-        return request.getHeader("auth");
+        return request.getHeader(HeaderName.AUTHORIZATION);
     }
 }
